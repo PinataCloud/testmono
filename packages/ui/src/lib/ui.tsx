143b978a0bcde5e6f8463c37f03f1ea6e2784ab1
Paulo Madronero <paulo@pinata.cloud>
2024-03-04 10:22:46 -0500

    feat: add image

--- packages/ui/src/lib/ui.tsx
+++ packages/ui/src/lib/ui.tsx
@@ -1,11 +1,12 @@
 import styles from './ui.module.css';
-
+import Image from '../images/image.svg';
 /* eslint-disable-next-line */
 export interface UiProps {}
 
 export function Ui(props: UiProps) {
   return (
     <div>
+      <Image />
       <h1>Welcome to Ui!</h1>
     </div>
   );
